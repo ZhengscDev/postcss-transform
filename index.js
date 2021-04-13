80032e55843715cfb0aa85293c2a07b339eadfe4
zhengshichao1 <zhengshichao1@jd.com>
2021-04-13 15:13:19 +0800

    完善项目

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const defaults = {
   replace: true,
   mediaQuery: false,
   minPixelValue: 0,
-  exclude: null
+  exclude: null,
+  targetUnit: 'rem'
 };
 
 const legacyOptions = {
@@ -43,13 +44,13 @@ function convertLegacyOptions(options) {
   });
 }
 
-function createPxReplace(rootValue, unitPrecision, minPixelValue) {
+function createPxReplace(rootValue, unitPrecision, minPixelValue, targetUnit) {
   return (m, $1) => {
     if (!$1) return m;
     const pixels = parseFloat($1);
     if (pixels < minPixelValue) return m;
     const fixedVal = toFixed(pixels / rootValue, unitPrecision);
-    return fixedVal === 0 ? "0" : fixedVal + "rem";
+    return fixedVal === 0 ? "0" : fixedVal + targetUnit;
   };
 }
 
@@ -145,7 +146,8 @@ module.exports = (options = {}) => {
       pxReplace = createPxReplace(
         rootValue,
         opts.unitPrecision,
-        opts.minPixelValue
+        opts.minPixelValue,
+        opts.targetUnit
       );
     },
     Declaration(decl) {
